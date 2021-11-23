4effb5154fcda89d4e593e923c744b8e62e0c4e6
sanni11 <91444352+sanni11@users.noreply.github.com>
2021-11-23 14:10:57 -0500

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,6 @@
 create table 
+--Apartment details (32)- Atharva
+--lease, payment and booking (37) - Sannidhi
+--User details, user login, contractor, management and maintenance (37) - Kshitija
+--Tenant, Notification, complaint and laundry (35) - Shabana
+--Total : 141 columns

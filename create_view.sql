8ac6a560284910274822786fdc75aed76f334acf
kshiti01 <91388153+kshiti01@users.noreply.github.com>
2021-11-25 14:08:57 -0500

    Update create_view.sql

--- create_view.sql
+++ create_view.sql
@@ -7,3 +7,10 @@ select ul.username, ul.password, us.user_type, us.last_login
 from user_login ul inner join user_details us
 on ul.usid = us.usid
 where us.user_type = 'b';
+
+
+create view  view_login_usertype_admin as 
+select ul.username, ul.password, us.user_type, us.last_login
+from user_login ul inner join user_details us
+on ul.usid = us.usid
+where us.user_type = 'a';

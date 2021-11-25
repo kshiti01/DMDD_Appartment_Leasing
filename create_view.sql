d39adbef89104ebe8aa5d2388ea344b6588cfb77
kshiti01 <91388153+kshiti01@users.noreply.github.com>
2021-11-25 14:06:37 -0500

    Update create_view.sql

--- create_view.sql
+++ create_view.sql
@@ -1,1 +1,9 @@
-create table 
+create views
+
+#### Kshitija
+
+create view  view_login_usertype_broker as 
+select ul.username, ul.password, us.user_type, us.last_login
+from user_login ul inner join user_details us
+on ul.usid = us.usid
+where us.user_type = 'b';

752f1ac45e28ee1ff465d2debec0150902f66063
kshiti01 <91388153+kshiti01@users.noreply.github.com>
2021-12-05 16:53:47 -0500

    Update insert_table.sql

--- insert_table.sql
+++ insert_table.sql
@@ -3,3 +3,6 @@ insert rows
 ###Kshitija
 USER_DETAILS
 insert into user_details values ('301','Mark', 'Williams', '8123458971', 'mark@gmail.com', 'Apt 43', '1132 Jersey St', 'Boston', '02231', 'Massachusets', 'US', TO_DATE('03/02/2001', 'DD/MM/YYYY' ),'M', 'T',TO_DATE('01/04/2021', 'DD/MM/YYYY'));
+
+MANAGEMENT_DETAILS
+insert into management_details values ('501','RealEstate Properties', 'Y',TO_DATE('03/02/2001', 'DD/MM/YYYY' ));

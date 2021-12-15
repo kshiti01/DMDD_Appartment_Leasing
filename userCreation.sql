aadb5e06f75e795f1613a6adacdf05708bbafe65
kshiti01 <91388153+kshiti01@users.noreply.github.com>
2021-12-15 07:50:50 -0500

    Create userCreation.sql

---
+++ userCreation.sql
@@ -0,0 +1,18 @@
+Create user testUser identified by InfoDmdd123456;
+GRANT R_USER TO testUser;
+
+
+Create user testTenant identified by InfoDmdd123456;
+GRANT TENANT TO testTenant;
+
+
+Create user testBroker identified by InfoDmdd123456;
+GRANT BROKER TO testBroker;
+
+
+Create user testMgmt identified by InfoDmdd123456;
+GRANT MANAGEMENT TO testMgmt;
+
+
+Create user testCon identified by InfoDmdd123456;
+GRANT CONTRACTOR TO testCon;

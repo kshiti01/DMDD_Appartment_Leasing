2c941e532ccee9aa031b9dcf4f621d8c47f75371
kshiti01 <91388153+kshiti01@users.noreply.github.com>
2021-12-14 19:11:12 -0500

    Update create_view.sql

--- create_view.sql
+++ create_view.sql
@@ -2,36 +2,58 @@ create views
 
 #### Kshitija
 
-create view  view_login_usertype_broker as 
+create view OR REPLACE view_login_usertype_broker as 
 select ul.username, ul.password, us.user_type, us.last_login
 from user_login ul inner join user_details us
 on ul.usid = us.usid
-where us.user_type = 'b';
+where us.user_type = 'B';
 
 
-create view  view_login_usertype_admin as 
+/*create view OR REPLACE  view_login_usertype_admin as 
 select ul.username, ul.password, us.user_type, us.last_login
 from user_login ul inner join user_details us
 on ul.usid = us.usid
-where us.user_type = 'a';
+where us.user_type = 'A';*/
 
-############## Atharva
+create view OR REPLACE  view_login_usertype_management as 
+select ul.username, ul.password, us.user_type, us.last_login
+from user_login ul inner join user_details us
+on ul.usid = us.usid
+where us.user_type = 'M';
+
 
 CREATE OR REPLACE FORCE EDITIONABLE VIEW "ADMIN"."VIEW_LOGIN_USERTYPE_USER" ("USERNAME", "PASSWORD", "USER_TYPE", "LAST_LOGIN") DEFAULT COLLATION "USING_NLS_COMP"  AS 
 SELECT ul.username, ul.password, us.user_type, us.last_login
 FROM user_login ul inner join user_details us
 on ul.usid = us.usid
-where us.user_type = 'u';
+where us.user_type = 'U';
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "ADMIN"."VIEW_LOGIN_USERTYPE_TENANT" ("USERNAME", "PASSWORD", "USER_TYPE", "LAST_LOGIN") DEFAULT COLLATION "USING_NLS_COMP"  AS 
+SELECT ul.username, ul.password, us.user_type, us.last_login
+FROM user_login ul inner join user_details us
+on ul.usid = us.usid
+where us.user_type = 'T';
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "ADMIN"."VIEW_LOGIN_USERTYPE_CONTRACTOR" ("USERNAME", "PASSWORD", "USER_TYPE", "LAST_LOGIN") DEFAULT COLLATION "USING_NLS_COMP"  AS 
+SELECT ul.username, ul.password, us.user_type, us.last_login
+FROM user_login ul inner join user_details us
+on ul.usid = us.usid
+where us.user_type = 'C';
+
+
+
+#############################################################################################
+
 
 CREATE OR REPLACE FORCE EDITIONABLE VIEW "ADMIN"."VIEW_LEASE_CHANGE_TENANT_DETAILS" ("USERNAME", "PASSWORD", "USER_TYPE", "FIRST_NAME", "LAST_NAME", "CONTACT_NO", "EMAIL", "ADDRESS_LINE1", "ADDRESS_LINE2", "CITY", "ZIPCODE", "COUNTRY", "RESIDENCE_STATE") DEFAULT COLLATION "USING_NLS_COMP"  AS 
 select ul.username, ul.password, us.user_type, us.first_name, us.last_name, us.contact_no, us.email, us.address_line1, us.address_line2, us.city, us.zipcode, us.country, us.residence_state
 from user_login ul inner join user_details us
 on ul.usid = us.usid
-where us.user_type = 't';
+where us.user_type = 'T';
 
 CREATE OR REPLACE FORCE EDITIONABLE VIEW "ADMIN"."VIEW_ADMIN_DETAILS" ("USERNAME", "PASSWORD", "USER_TYPE", "FIRST_NAME", "LAST_NAME", "CONTACT_NO", "EMAIL", "ADDRESS_LINE1", "ADDRESS_LINE2", "CITY", "ZIPCODE", "COUNTRY", "RESIDENCE_STATE") DEFAULT COLLATION "USING_NLS_COMP"  AS 
 select ul.username, ul.password, us.user_type, us.first_name, us.last_name, us.contact_no, us.email, us.address_line1, us.address_line2, us.city, us.zipcode, us.country, us.residence_state
 from user_login ul inner join user_details us
 on ul.usid = us.usid
-where us.user_type = 'a';
+where us.user_type = 'C';
 
